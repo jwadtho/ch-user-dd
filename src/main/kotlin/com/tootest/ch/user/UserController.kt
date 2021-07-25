ad370a8f94a5e3c9427adc9991bfdf7f9b1951b2
Jannarong Wadthong <toojannarong@gmail.com>
2021-07-25 22:15:13 +0700

    print out http request headers

--- src/main/kotlin/com/tootest/ch/user/UserController.kt
+++ src/main/kotlin/com/tootest/ch/user/UserController.kt
@@ -3,6 +3,7 @@ package com.tootest.ch.user
 import com.tootest.ch.user.dto.UserResponse
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,10 +14,12 @@ class UserController {
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @GetMapping("/user")
-    fun user(@RequestParam email: String) : UserResponse {
+    fun user(@RequestParam email: String, @RequestHeader headers: Map<String, String>) : UserResponse {
+        logger.info("Request Headers:{}", headers.entries.joinToString { it.key + ":" + it.value })
         logger.info("[UserInfo] Retrieving user info: email={}", email)
         val userInfo =  UserResponse( email, "Name1", "lastName1")
         logger.info("[UserInfo] User Info:{}", userInfo)
+
         return userInfo
     }
 
