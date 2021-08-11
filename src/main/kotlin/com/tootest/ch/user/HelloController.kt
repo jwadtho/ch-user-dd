e487c8af3496d1efc7d7cc10cdb4da2c7a689164
Jannarong Wadthong <toojannarong@gmail.com>
2021-08-12 03:37:39 +0700

    add hello

--- src/main/kotlin/com/tootest/ch/user/HelloController.kt
+++ src/main/kotlin/com/tootest/ch/user/HelloController.kt
@@ -1,3 +1,5 @@
+package com.tootest.ch.user
+
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
