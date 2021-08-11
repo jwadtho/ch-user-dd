c1bb7f8adfeb0728a5fd175ae4707f48ad05f672
Jannarong Wadthong <toojannarong@gmail.com>
2021-08-12 03:30:46 +0700

    add hello

---
+++ src/main/kotlin/HelloController.kt
@@ -0,0 +1,16 @@
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class HelloController {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @GetMapping("/hi")
+    fun hello() : String {
+        logger.info("Hi, there")
+        return "Hi, there"
+    }
+}
